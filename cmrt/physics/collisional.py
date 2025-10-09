4b00319fa30d929c9b126655dc066fd6764cdc53
htjb <harrytjbevins@yahoo.co.uk>
2025-10-09 20:47:30 +0100

    condensing code

--- cmrt/physics/collisional.py
+++ cmrt/physics/collisional.py
@@ -2,16 +2,16 @@ import jax.numpy as jnp
 import jax
 from cmrt.utils.utils import const
 
-def n_H_tot(z, cosmo):
+def n_H_tot(z, cosmo, yhe):
     # mean hydrogen number density in m^-3
     H0 = cosmo.H0 * 1e3 / 3.086e22
     rho_bar = 3/8*H0**2/jnp.pi/const.G*cosmo.Omega_b*(1+z)**3
-    return (const.X_H * rho_bar) / const.m_p
+    return ((1-yhe) * rho_bar) / const.m_p
 
 @jax.jit
-def xc(z, xe, Tk, cosmo):
+def xc(z, xe, Tk, cosmo, yhe):
 
-    nH = n_H_tot(z, cosmo)
+    nH = n_H_tot(z, cosmo, yhe)
 
     xc = const.Tstar / (const.A10 * const.Tcmb0*(1+z)) * kappa(z, Tk, xe) * nH *(1- xe)
 
@@ -29,3 +29,21 @@ def kappa(z, Tk, xe):
     kappa_pH = kappa_eH  
 
     return kappa_HH * (1 - xe) + (kappa_eH + kappa_pH) * xe
+
+
+@jax.jit
+def Tcmb(z):
+    T = const.Tcmb0 * (1 + z)  # CMB temperature in Kelvin, scaled by redshift
+    return T
+
+def Ts(z, T_gas, T_cmb, xc):
+    """ Calculate spin temperature Tspin."""
+    Tspin = (T_cmb**(-1) + xc*T_gas**(-1))/(1+xc)
+    return Tspin**(-1)
+
+def T21(z, T_gas, T_cmb, T_s, xe, cosmo, yhe):
+    """Calculate 21cm brightness temperature."""
+    return 27*(1-xe)*((1-yhe)/0.76)* \
+        (cosmo.Omega_b*(cosmo.H0/100)**2)/0.023 * \
+            jnp.sqrt(0.15/(cosmo.Omega_m*(cosmo.H0/100)**2)*(1+z/10)) * \
+                (1 - T_cmb/T_s)  # 21cm brightness temperature in mKelvin
