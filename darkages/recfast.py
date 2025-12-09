ab751f37911ce9da967761065660242fda81db3b
htjb <harrytjbevins@yahoo.co.uk>
2025-12-09 10:56:46 +0000

    spotted an error in how recfast was being called

--- darkages/recfast.py
+++ darkages/recfast.py
@@ -1,13 +1,22 @@
-import re
+"""A wrapper for Recfast++."""
+
 import os
+import re
+from typing import NamedTuple
+
 import numpy as np
 
-def update_recfast_ini(cosmo, base_dir='./'):
-    """
+
+def update_recfast_ini(cosmo: NamedTuple, base_dir: str = "./") -> None:
+    """Build the Recfast++ .ini file.
+
     Update H0, Omega_b, Omega_c, and Y_He in a Recfast++ .ini file.
-    Saves modified version to `path_out`.
+
+    Args:
+        cosmo: Cosmology namedtuple with attributes H0, Omega_b, Omega_c, Y_He.
+        base_dir: Directory to save the modified ini file.
     """
-    with open('recfast-.vx/runfiles/parameters.ini', "r") as f:
+    with open("recfast-.vx/runfiles/parameters.ini", "r") as f:
         lines = f.readlines()
 
     # Compute derived parameter
@@ -18,7 +27,7 @@ def update_recfast_ini(cosmo, base_dir='./'):
         r"^(Yp\s*=\s*)([0-9Ee\.\+\-]+)": f"\\g<1>{cosmo.Y_He}",
         r"^(Omega_b\s*=\s*)([0-9Ee\.\+\-]+)": f"\\g<1>{cosmo.Omega_b}",
         r"^(Omega_m\s*=\s*)([0-9Ee\.\+\-]+)": f"\\g<1>{omega_m}",
-        r"^(h100\s*=\s*)([0-9Ee\.\+\-]+)": f"\\g<1>{cosmo.H0/100.0}",
+        r"^(h100\s*=\s*)([0-9Ee\.\+\-]+)": f"\\g<1>{cosmo.H0 / 100.0}",
     }
 
     new_lines = []
@@ -28,24 +37,40 @@ def update_recfast_ini(cosmo, base_dir='./'):
             new_line = re.sub(pattern, repl, new_line)
         new_lines.append(new_line)
 
-    with open(base_dir + 'recfast_input.ini', "w") as f:
+    with open(base_dir + "recfast_input.ini", "w") as f:
         f.writelines(new_lines)
 
-def call_recfast(base_dir='./', redshift=1100):
-    """
-    Code to call recfast.
-    """
 
-    
-    os.system('recfast-.vx/Recfas++ < ' + base_dir + '/recfast_input.ini' + '> /dev/null 2>&1')
-    
-    
-    data = np.loadtxt('recfast-output/Xe_Recfast++.Rec_corrs_CT2010.dat', skiprows=4)
-    recz = data[:,0][::-1]
-    xe = data[:,1][::-1]
-    Tk = data[:,4][::-1]
+def call_recfast(
+    base_dir: str = "./", redshift: int = 1100
+) -> tuple[np.ndarray, np.ndarray]:
+    """Code to call recfast.
+
+    Code runs the recfast executable.
+
+    Args:
+        base_dir: Directory where the recfast_input.ini file is located.
+        redshift: Redshift at which to interpolate the output.
+
+    Returns:
+        init_xe: Interpolated free electron fraction at the given redshift.
+        init_Tk: Interpolated gas temperature at the given redshift.
+    """
+    os.system(
+        "./recfast-.vx/Recfast++ "
+        + base_dir
+        + "/recfast_input.ini"
+        + "> /dev/null 2>&1"
+    )
+
+    data = np.loadtxt(
+        "recfast-output/Xe_Recfast++.Rec_corrs_CT2010.dat", skiprows=4
+    )
+    recz = data[:, 0][::-1]
+    xe = data[:, 1][::-1]
+    Tk = data[:, 4][::-1]
 
     init_xe = np.interp(redshift, recz, xe)
-    init_Tk = np.interp(redshift, recz, Tk) # in Kelvin
+    init_Tk = np.interp(redshift, recz, Tk)  # in Kelvin
 
     return init_xe, init_Tk
